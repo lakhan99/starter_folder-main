70edc5e76c9faf5d21d7a5e8b10b63368cb81498
Arsh Lakhanpal <arsh.lakhanpal@mail.utoronto.ca>
2022-02-07 04:48:30 +0000

    Final Commit at 11:48pm

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # starter_folder
 
-This repo....
+This repository explores the factors that affect covid cases and also gives information on which age groups are most affected in terms of infection of the virus. I was able to observe how like any virus, COVID-19, although prominent throughout the year, begins to thrive and affect more people during the colder times of the year. The dataset was called "COVID-19 Cases in Toronto" and was taken from the Open Data Toronto Portal.   
 
 It is organised as follows...
+
+Under outputs, there is the PDF that I will submit along with the RMD so that anyone is able to see my code and the References.Bib file that was used for my in-text citations and references at the end of my paper. Under inputs, I have my raw data that was taken from the portal and worked with for this paper.  
--- scripts/01-data_download.R
+++ scripts/01-data_download.R
@@ -1,14 +1,10 @@
 #### Preamble ####
 # Purpose: Download data from opendatatoronto 
 # Author: Arsh Lakhanpal 
-# Data: 3 January 2022
+# Data: 6 February 2022
 # Contact: arsh.lakhanpal@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
+
 
 
 #### Workspace setup ####
@@ -31,12 +27,12 @@ resources <- list_package_resources("64b54586-6180-4485-83eb-81e8fae3b8fe")
 # identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
 datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 
-#There is only one rsource and so get_resource() will load that
+#There is only one resource and so get_resource() will load that
 covid_cases <-
   filter(datastore_resources, row_number() == 1) %>% get_resource()
 
 
-#### Save data #### 
+#### Save data ##### 
 
 write_csv(covid_cases, "inputs/data/covid_cases.csv")
        
